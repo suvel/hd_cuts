db07fc7d8001cff57c321ee75a21eef033f1cdf5
Suvel Rathneswar <find.suvel@gmail.com>
2022-01-14 04:32:09 +0530

    Video Annimation - updated

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,24 +3,23 @@ import "./Videos.css";
 import GlobalContext from "../context/GlobalContext";
 
 function Video({ style, triggerCascade }) {
-  const { vMode } = useContext(GlobalContext);
-  return (
-    <div className={`Video ${vMode}`} style={style} onClick={triggerCascade} />
-  );
+  return <div className={`Video`} style={style} onClick={triggerCascade} />;
 }
 //styles={marginLeft: "10px", marginTop: "10px" }
 
 function Videos() {
-  const { vModeTypes, setVMode } = useContext(GlobalContext);
+  const { vMode, vModeTypes, setVMode } = useContext(GlobalContext);
   return (
-    <div className="Videos">
-      <Video style={{ marginLeft: "40px", marginTop: "40px" }} />
-      <Video style={{ marginLeft: "20px", marginTop: "20px" }} />
-      <Video
-        triggerCascade={() => {
-          setVMode(vModeTypes.CASCADE);
-        }}
-      />
+    <div className={`Videos`}>
+      <div className={`Video_container ${vMode}`}>
+        <Video style={{ marginLeft: "40px", marginTop: "40px" }} />
+        <Video style={{ marginLeft: "20px", marginTop: "20px" }} />
+        <Video
+          triggerCascade={() => {
+            setVMode(vModeTypes.CASCADE);
+          }}
+        />
+      </div>
     </div>
   );
 }
